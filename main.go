f3047b2d21ab52209508a4333c70152813be7bd2
Michael Penkov <m@penkov.dev>
2022-01-30 22:04:14 +0900

    working pagination and templates

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ package main
 
 I want a summary of my activity on github in 2021.
 
-- PRs pushed
-- PRs reviewed
-- Issues opened and closed
+- [x] PRs pushed
+- [x] PRs reviewed
+- [ ] Issues opened and closed
 
 Looks like this may be difficult to do across all repos.
 Perhaps we can just target one repo at a time?
@@ -17,11 +17,14 @@ I mostly know the repos I worked on during the year.
 import (
     "encoding/json"
     "fmt"
+    "html/template"
     "io"
     "net/http"
     "log"
     "os"
+    "sort"
     "strings"
+    "time"
 )
 
 // Having to define these up front is a pain...
@@ -41,16 +44,125 @@ type Commit struct {
 type Pull struct {
     Number int
     HtmlUrl string `json:"html_url"`
+    CreatedAt string `json:"created_at"`
     MergedAt string `json:"merged_at"`
     State string
     Title string
     User User
 
-    myContribution string
+    MyContribution string
+    Timestamp string
 }
 
 //
-// The cache functions work around Github API rate limiting
+// For sorting
+//
+type PullList []Pull
+
+func (pl PullList) Len() int {
+    return len(pl)
+}
+
+func (pl PullList) Less(i, j int) bool {
+    return pl[i].Number > pl[j].Number
+}
+
+func (pl PullList) Swap(i, j int) {
+    pl[i], pl[j] = pl[j], pl[i]
+}
+
+type RepoResult struct {
+    Name string
+    Pulls []Pull
+    Authored int
+    Merged int
+}
+
+const header string = `<html>
+<head>
+<style>
+body {
+    font-family: sans-serif;
+}
+
+table thead th {
+    text-align: left;
+}
+
+table {
+    border-collapse: collapse;
+    text-align: left;
+    vertical-align: middle;
+}
+
+tbody tr:nth-child(odd) {
+    background-color: hsl(0, 100%, 100%);
+}
+
+tbody tr:nth-child(even) {
+    background-color: hsl(0, 0%, 90%);
+}
+
+th, td {
+    border: 1px solid black;
+    padding: 5px;
+}
+
+td.state-open {
+    color: hsl(0, 90%, 50%);
+}
+td.state-closed {
+    color: hsl(240, 100%, 50%);
+}
+
+td.contribution-authored {
+    color: hsl(120, 80%, 40%);
+}
+td.contribution-merged {
+    color: hsl(240, 100%, 50%);
+}
+
+td {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    max-width: 400px;
+}
+</style>
+</head>
+<body>`
+
+const templ string = `
+<h1>{{ .Name }}</h1>
+<p>Authored {{ .Authored }} and merged {{ .Merged }} contributions.</p>
+<table>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Timestamp</th>
+            <th>State</th>
+            <th>Contribution</th>
+            <th>Title</th>
+        </tr>
+    </thead>
+    <tbody>
+    {{ range .Pulls }}
+        <tr>
+            <td><a href="{{ .HtmlUrl }}">{{ .Number }}</a></td>
+            <td>{{ .Timestamp }}</td>
+            <td class="state-{{ .State }}">{{ .State }}</td>
+            <td class="contribution-{{ .MyContribution }}">{{ .MyContribution }}</td>
+            <td><a href="{{ .HtmlUrl }}">{{ .Title }}</a></td>
+        </tr>
+    {{ end }}
+    </tbody>
+</table>
+`
+
+var report = template.Must(template.New("issuelist").Parse(templ))
+
+//
+// The cache functions are yet another work-around for Github API rate limiting
 //
 func readCache(path string) (data []byte, err error) {
     file, err := os.Open(path)
@@ -90,9 +202,15 @@ func httpGet(url string) []byte {
     if (resp.StatusCode > 299) {
         log.Fatalf("HTTP %d", resp.StatusCode)
     }
+    //
+    // Prevent us from getting rate-limited
+    //
+    time.Sleep(5000 * time.Millisecond)
     return body
 }
 
+// This loadX stuff is rather repetitive, is there a way to avoid duplicating
+// it for each of Commit, Pull and Event?
 func loadCommit(repo string, sha string) Commit {
     var commit Commit
 
@@ -103,9 +221,10 @@ func loadCommit(repo string, sha string) Commit {
         return commit
     }
 
-    log.Printf("cache miss, reading from the wire")
+    url := fmt.Sprintf("https://api.github.com/repos/%s/commits/%s", repo, sha)
+    log.Printf("cache miss, reading %s from the wire", url)
 
-    body := httpGet(fmt.Sprintf("https://api.github.com/repos/%s/commits/%s", repo, sha))
+    body := httpGet(url)
     writeCache(jsonFilename, body)
 
     if err := json.Unmarshal(body, &commit); err != nil {
@@ -125,9 +244,10 @@ func loadEvents(repo string, issueNumber int) []Event {
         return events
     }
 
-    log.Printf("cache miss, reading from the wire")
+    url := fmt.Sprintf("https://api.github.com/repos/%s/issues/%d/events", repo, issueNumber)
+    log.Printf("cache miss, reading %s from the wire", url)
+    data = httpGet(url)
 
-    data = httpGet(fmt.Sprintf("https://api.github.com/repos/%s/issues/%d/events", repo, issueNumber))
     writeCache(jsonFilename, data)
 
     if err := json.Unmarshal(data, &events); err != nil {
@@ -147,12 +267,13 @@ func loadPulls(repo string, page int) []Pull {
         return pulls
     }
 
-    log.Printf("cache miss, reading from the wire")
+    url := fmt.Sprintf("https://api.github.com/repos/%s/pulls?state=all&page=%d", repo, page)
+    log.Printf("cache miss, reading %s from the wire", url)
 
-    body := httpGet(fmt.Sprintf("https://api.github.com/repos/%s/pulls?state=all", repo))
-    writeCache(jsonFilename, body)
+    data = httpGet(url)
+    writeCache(jsonFilename, data)
 
-    if err := json.Unmarshal(body, &pulls); err != nil {
+    if err := json.Unmarshal(data, &pulls); err != nil {
         log.Fatalf("JSON unmarshalling failed: %s", err)
     }
 
@@ -168,26 +289,61 @@ func whoMerged(repo string, issueNumber int) User {
     return User{"nobody"}
 }
 
+func parseTime(pull Pull) time.Time {
+    const format string = "2006-01-02T15:04:05Z"
+    parsedTime, err := time.Parse(format, pull.CreatedAt)
+    if (err != nil) {
+        log.Fatalf("unable to parse time from %s", pull.CreatedAt)
+    }
+    return parsedTime
+}
+
 func main() {
     var repos = os.Args[1:]
+    fmt.Println(header)
     for _, repo := range(repos) {
-        // TODO: pagination
-        pulls := loadPulls(repo, 1)
-        for _, p := range(pulls) {
-            //
-            // For each pull request, we need to work out what our contribution,
-            // if any, actually was.  Did we actually author the PR?  Or did we
-            // simply merge it?
-            //
-            if p.User.Login == "mpenkov" {
-                p.myContribution = "authored"
-            } else if p.State == "closed" && whoMerged(repo, p.Number).Login == "mpenkov" {
-                p.myContribution = "merged"
+        var pulls []Pull
+        var done bool = false
+        var authored int = 0
+        var merged int = 0
+        for page := 1; !done; page++ {
+            pagePulls := loadPulls(repo, page)
+            if len(pagePulls) == 0 {
+                break
             }
+            sort.Sort(PullList(pagePulls))
 
-            if p.myContribution != "" {
-                fmt.Printf("%d. [%s] %s %s %s\n", p.Number, p.myContribution, p.MergedAt, p.Title, p.HtmlUrl)
+            for _, p := range(pagePulls) {
+                ts := parseTime(p)
+                if ts.Year() > 2021 {
+                    continue
+                } else if ts.Year() < 2021 {
+                    done = true
+                    break
+                }
+                p.Timestamp = ts.Format("2006-01-02")
+
+                //
+                // For each pull request, we need to work out what our contribution,
+                // if any, actually was.  Did we actually author the PR?  Or did we
+                // simply merge it?
+                //
+                if p.User.Login == "mpenkov" {
+                    p.MyContribution = "authored"
+                    authored++
+                } else if p.State == "closed" && whoMerged(repo, p.Number).Login == "mpenkov" {
+                    p.MyContribution = "merged"
+                    merged++
+                } else {
+                    continue
+                }
+
+                pulls = append(pulls, p)
             }
+        }
+
+        if err := report.Execute(os.Stdout, RepoResult{repo, pulls, authored, merged}); err != nil {
+            log.Fatal(err)
         }
     }
 }
