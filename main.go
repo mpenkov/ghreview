39c25a0d8e00cb5c3ebaed5dc14874b97cef5907
Michael Penkov <m@penkov.dev>
2022-01-30 22:08:17 +0900

    go fmt

--- main.go
+++ main.go
@@ -15,39 +15,39 @@ I mostly know the repos I worked on during the year.
 */
 
 import (
-    "encoding/json"
-    "fmt"
-    "html/template"
-    "io"
-    "net/http"
-    "log"
-    "os"
-    "sort"
-    "strings"
-    "time"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"time"
 )
 
 // Having to define these up front is a pain...
 type User struct {
-    Login string
+	Login string
 }
 
 type Event struct {
-    Actor User
-    Event string
+	Actor User
+	Event string
 }
 
 type Pull struct {
-    Number int
-    HtmlUrl string `json:"html_url"`
-    CreatedAt string `json:"created_at"`
-    MergedAt string `json:"merged_at"`
-    State string
-    Title string
-    User User
+	Number    int
+	HtmlUrl   string `json:"html_url"`
+	CreatedAt string `json:"created_at"`
+	MergedAt  string `json:"merged_at"`
+	State     string
+	Title     string
+	User      User
 
-    MyContribution string
-    Timestamp string
+	MyContribution string
+	Timestamp      string
 }
 
 //
@@ -56,22 +56,22 @@ type Pull struct {
 type PullList []Pull
 
 func (pl PullList) Len() int {
-    return len(pl)
+	return len(pl)
 }
 
 func (pl PullList) Less(i, j int) bool {
-    return pl[i].Number > pl[j].Number
+	return pl[i].Number > pl[j].Number
 }
 
 func (pl PullList) Swap(i, j int) {
-    pl[i], pl[j] = pl[j], pl[i]
+	pl[i], pl[j] = pl[j], pl[i]
 }
 
 type RepoResult struct {
-    Name string
-    Pulls []Pull
-    Authored int
-    Merged int
+	Name     string
+	Pulls    []Pull
+	Authored int
+	Merged   int
 }
 
 const header string = `<html>
@@ -161,162 +161,162 @@ var report = template.Must(template.New("issuelist").Parse(templ))
 // The cache functions are yet another work-around for Github API rate limiting
 //
 func readCache(path string) (data []byte, err error) {
-    file, err := os.Open(path)
-    if err != nil {
-        return nil, err
-    }
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
 
-    data, err = io.ReadAll(file)
-    if err != nil {
-        return nil, err
-    }
+	data, err = io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
 
-    file.Close()
-    return data, nil
+	file.Close()
+	return data, nil
 }
 
 func writeCache(path string, data []byte) {
-    // Why can't I just iterate over the chars and compare them to "/"?
-    subdir := path[:strings.LastIndex(path, "/")]
-    err := os.MkdirAll(subdir, 0700)
-    if err != nil {
-        log.Fatalf("could not mkdir %s", subdir)
-    }
-    err = os.WriteFile(path, data, 0700)
-    if err != nil {
-        log.Fatalf("unable to write to %s", path)
-    }
+	// Why can't I just iterate over the chars and compare them to "/"?
+	subdir := path[:strings.LastIndex(path, "/")]
+	err := os.MkdirAll(subdir, 0700)
+	if err != nil {
+		log.Fatalf("could not mkdir %s", subdir)
+	}
+	err = os.WriteFile(path, data, 0700)
+	if err != nil {
+		log.Fatalf("unable to write to %s", path)
+	}
 }
 
 func httpGet(url string) []byte {
-    resp, err := http.Get(url)
-    if err != nil {
-        log.Fatal(err)
-    }
-    body, err := io.ReadAll(resp.Body)
-    resp.Body.Close()
-    if (resp.StatusCode > 299) {
-        log.Fatalf("HTTP %d", resp.StatusCode)
-    }
-    //
-    // Prevent us from getting rate-limited
-    //
-    time.Sleep(5000 * time.Millisecond)
-    return body
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if resp.StatusCode > 299 {
+		log.Fatalf("HTTP %d", resp.StatusCode)
+	}
+	//
+	// Prevent us from getting rate-limited
+	//
+	time.Sleep(5000 * time.Millisecond)
+	return body
 }
 
 // This loadX stuff is rather repetitive, is there a way to avoid duplicating
 // it for each of Pull and Event?
 func loadEvents(repo string, issueNumber int) []Event {
-    var events []Event
+	var events []Event
 
-    jsonFilename := fmt.Sprintf("cache/%s/events/%d.json", repo, issueNumber)
-    data, err := readCache(jsonFilename)
-    if err == nil {
-        json.Unmarshal(data, &events)
-        return events
-    }
+	jsonFilename := fmt.Sprintf("cache/%s/events/%d.json", repo, issueNumber)
+	data, err := readCache(jsonFilename)
+	if err == nil {
+		json.Unmarshal(data, &events)
+		return events
+	}
 
-    url := fmt.Sprintf("https://api.github.com/repos/%s/issues/%d/events", repo, issueNumber)
-    log.Printf("cache miss, reading %s from the wire", url)
-    data = httpGet(url)
+	url := fmt.Sprintf("https://api.github.com/repos/%s/issues/%d/events", repo, issueNumber)
+	log.Printf("cache miss, reading %s from the wire", url)
+	data = httpGet(url)
 
-    writeCache(jsonFilename, data)
+	writeCache(jsonFilename, data)
 
-    if err := json.Unmarshal(data, &events); err != nil {
-        log.Fatalf("JSON unmarshalling failed: %s", err)
-    }
+	if err := json.Unmarshal(data, &events); err != nil {
+		log.Fatalf("JSON unmarshalling failed: %s", err)
+	}
 
-    return events
+	return events
 }
 
 func loadPulls(repo string, page int) []Pull {
-    var pulls []Pull
+	var pulls []Pull
 
-    jsonFilename := fmt.Sprintf("cache/%s/pulls/%d.json", repo, page)
-    data, err := readCache(jsonFilename)
-    if err == nil {
-        json.Unmarshal(data, &pulls)
-        return pulls
-    }
+	jsonFilename := fmt.Sprintf("cache/%s/pulls/%d.json", repo, page)
+	data, err := readCache(jsonFilename)
+	if err == nil {
+		json.Unmarshal(data, &pulls)
+		return pulls
+	}
 
-    url := fmt.Sprintf("https://api.github.com/repos/%s/pulls?state=all&page=%d", repo, page)
-    log.Printf("cache miss, reading %s from the wire", url)
+	url := fmt.Sprintf("https://api.github.com/repos/%s/pulls?state=all&page=%d", repo, page)
+	log.Printf("cache miss, reading %s from the wire", url)
 
-    data = httpGet(url)
-    writeCache(jsonFilename, data)
+	data = httpGet(url)
+	writeCache(jsonFilename, data)
 
-    if err := json.Unmarshal(data, &pulls); err != nil {
-        log.Fatalf("JSON unmarshalling failed: %s", err)
-    }
+	if err := json.Unmarshal(data, &pulls); err != nil {
+		log.Fatalf("JSON unmarshalling failed: %s", err)
+	}
 
-    return pulls
+	return pulls
 }
 
 func whoMerged(repo string, issueNumber int) User {
-    for _, event := range(loadEvents(repo, issueNumber)) {
-        if event.Event == "merged" {
-            return event.Actor
-        }
-    }
-    return User{"nobody"}
+	for _, event := range loadEvents(repo, issueNumber) {
+		if event.Event == "merged" {
+			return event.Actor
+		}
+	}
+	return User{"nobody"}
 }
 
 func parseTime(pull Pull) time.Time {
-    const format string = "2006-01-02T15:04:05Z"
-    parsedTime, err := time.Parse(format, pull.CreatedAt)
-    if (err != nil) {
-        log.Fatalf("unable to parse time from %s", pull.CreatedAt)
-    }
-    return parsedTime
+	const format string = "2006-01-02T15:04:05Z"
+	parsedTime, err := time.Parse(format, pull.CreatedAt)
+	if err != nil {
+		log.Fatalf("unable to parse time from %s", pull.CreatedAt)
+	}
+	return parsedTime
 }
 
 func main() {
-    var repos = os.Args[1:]
-    fmt.Println(header)
-    for _, repo := range(repos) {
-        var pulls []Pull
-        var done bool = false
-        var authored int = 0
-        var merged int = 0
-        for page := 1; !done; page++ {
-            pagePulls := loadPulls(repo, page)
-            if len(pagePulls) == 0 {
-                break
-            }
-            sort.Sort(PullList(pagePulls))
+	var repos = os.Args[1:]
+	fmt.Println(header)
+	for _, repo := range repos {
+		var pulls []Pull
+		var done bool = false
+		var authored int = 0
+		var merged int = 0
+		for page := 1; !done; page++ {
+			pagePulls := loadPulls(repo, page)
+			if len(pagePulls) == 0 {
+				break
+			}
+			sort.Sort(PullList(pagePulls))
 
-            for _, p := range(pagePulls) {
-                ts := parseTime(p)
-                if ts.Year() > 2021 {
-                    continue
-                } else if ts.Year() < 2021 {
-                    done = true
-                    break
-                }
-                p.Timestamp = ts.Format("2006-01-02")
+			for _, p := range pagePulls {
+				ts := parseTime(p)
+				if ts.Year() > 2021 {
+					continue
+				} else if ts.Year() < 2021 {
+					done = true
+					break
+				}
+				p.Timestamp = ts.Format("2006-01-02")
 
-                //
-                // For each pull request, we need to work out what our contribution,
-                // if any, actually was.  Did we actually author the PR?  Or did we
-                // simply merge it?
-                //
-                if p.User.Login == "mpenkov" {
-                    p.MyContribution = "authored"
-                    authored++
-                } else if p.State == "closed" && whoMerged(repo, p.Number).Login == "mpenkov" {
-                    p.MyContribution = "merged"
-                    merged++
-                } else {
-                    continue
-                }
+				//
+				// For each pull request, we need to work out what our contribution,
+				// if any, actually was.  Did we actually author the PR?  Or did we
+				// simply merge it?
+				//
+				if p.User.Login == "mpenkov" {
+					p.MyContribution = "authored"
+					authored++
+				} else if p.State == "closed" && whoMerged(repo, p.Number).Login == "mpenkov" {
+					p.MyContribution = "merged"
+					merged++
+				} else {
+					continue
+				}
 
-                pulls = append(pulls, p)
-            }
-        }
+				pulls = append(pulls, p)
+			}
+		}
 
-        if err := report.Execute(os.Stdout, RepoResult{repo, pulls, authored, merged}); err != nil {
-            log.Fatal(err)
-        }
-    }
+		if err := report.Execute(os.Stdout, RepoResult{repo, pulls, authored, merged}); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
